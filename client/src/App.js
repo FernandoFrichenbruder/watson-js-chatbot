875f270afed60cc433e4db4d3a13fd8a2b7b6397
Fernando Frichenbruder <fernando.frich@gmail.com>
2021-01-25 11:26:40 -0300

    Transfer session code from index.js to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,27 @@ import { Provider } from "react-redux";
 import store from "./store";
 import Chat from "./components/Chat";
 import { createSession } from "./actions/watson";
+import axios from "axios";
 
+if (localStorage.session) {
+  const session = localStorage.session;
+  localStorage.clear();
+  delete axios.defaults.headers.common["session_id"];
+  axios.defaults.headers.common["session_id"] = session;
+} else {
+  localStorage.clear();
+  delete axios.defaults.headers.common["session_id"];
+}
 
 const App = () => {
+
   useEffect(() => {
     if (!localStorage.session) {
       store.dispatch(createSession());
     }
+    
   });
+
   return (
     <Provider store={store}>
       <div className="container">
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import axios from "axios";
 
-if (localStorage.session) {
-  const session = localStorage.session;
-  localStorage.clear();
-  delete axios.defaults.headers.common["session_id"];
-  axios.defaults.headers.common["session_id"] = session;
-} else {
-  localStorage.clear();
-  delete axios.defaults.headers.common["session_id"];
-}
 ReactDOM.render(
   <React.StrictMode>
     <App />
