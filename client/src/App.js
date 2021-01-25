f1378c3b603f5e4fb2694d69f854fe81fcabfd97
Fernando Frichenbruder <fernando.frich@gmail.com>
2021-01-25 10:28:54 -0300

    Fix sessionId

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import Chat from "./components/Chat";
 import { createSession } from "./actions/watson";
-import axios from "axios";
 
-if (localStorage.session) {
-  const session = localStorage.session;
-  localStorage.clear();
-  delete axios.defaults.headers.common["session_id"];
-  axios.defaults.headers.common["session_id"] = session;
-} else {
-  localStorage.clear();
-  delete axios.defaults.headers.common["session_id"];
-}
 
 const App = () => {
   useEffect(() => {
