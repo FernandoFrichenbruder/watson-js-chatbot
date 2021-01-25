55833424cd0858cc80417831f9591dde37a9d657
Fernando Frichenbruder <fernando.frich@gmail.com>
2021-01-24 23:29:49 -0300

    Creat __tests__ dir for react

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 import axios from 'axios';
-import Message from './components/UI/molecules/Message';
+import Message from '../components/UI/molecules/Message';
 
 describe('fetchData', () => {
 
